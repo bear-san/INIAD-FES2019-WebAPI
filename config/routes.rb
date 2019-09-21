576c024de0db719bdba01f00866c6427b1acabe7
Kentaro <kentaro@whim.jp>
2019-09-21 23:21:29 +0900

    URL設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    scope :v1 do
+
+      scope :user do
+        post '/new', to:'user#new'
+        post '/', to:'user#update_notification_token'
+      end
+
+      scope :contents do
+        get '/', to:'contents#dump'
+      end
+    end
+  end
 end
