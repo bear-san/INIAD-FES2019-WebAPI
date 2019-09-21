884d3a16ac44d82149765d43506def1a5f3dc7c6
Kentaro <kentaro@whim.jp>
2019-09-21 22:24:44 +0900

    デバイストークン（通知用）アップデート device_typeは新規登録時にrequiredなので要求しない

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  before_action :authentication, :except => new
+
   def new
     if params["device_type"].present? then
       render json:{"status" => "error", "description" => "device_type is required"},status:400
@@ -9,10 +11,24 @@ class UserController < ApplicationController
     user.user_id = SecureRandom.uuid
     user.secret = SecureRandom.alphanumeric(32)
     user.role = ["participant"]
+    user.device_type = params["device_type"]
 
     user.save()
 
     render json:{"status" => "success", "secret" => user.secret, "role" => user.role}
     return
+  end
+
+  def update_notification_token
+    if params["device_token"].present? then
+      render json:{"status" => "error", "description" => "required parameters missing"},status:400
+    end
+
+    user = User.find_by_user_id(@user.user_id)
+    user.notification_token = params["device_token"]
+
+    user.save()
+
+    render json:{"status" => "success", "description" => "notification token update has been successfully"}
   end
 end
