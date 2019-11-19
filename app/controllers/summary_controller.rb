b0b4c096029b18effdfb83e2e9dfa0cddf2bcc06
Kentaro <kentaro@whim.jp>
2019-11-19 15:48:51 +0900

    時間帯別来場者数 テスト1

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -24,6 +24,20 @@ class SummaryController < ApplicationController
     target_content["visitors"] = visitors.uniq{|visitor| visitor["user"].user_id}.sort{|visitor1, visitor2| visitor1["timestamp"] <=> visitor2["timestamp"]}
 
     @contents = target_content
+    base_unix_time = [1572742800,1572829200]
+
+    @visitor_by_hours = [
+    ]
+
+    4.times do
+      base_unix_time[0] += 7200
+      @visitor_by_hours.append(target_content["visitors"].select{|visitor| Time.parse(visitor["timestamp"]) >= base_unix_time[0] and Time.parse(visitor["timestamp"]) < base_unix_time[0] + 7200}.count)
+    end
+
+    4.times do
+      base_unix_time[1] += 7200
+      @visitor_by_hours.append(target_content["visitors"].select{|visitor| Time.parse(visitor["timestamp"]) >= base_unix_time[1] and Time.parse(visitor["timestamp"]) < base_unix_time[1] + 7200}.count)
+    end
     #render json:{"status" => "success", "data" => target_content}
     return
 
